d4ad5f481496cd0429733f39b3dea948eb48a498
lllidan <lllidanismine@gmail.com>
2017-09-20 23:22:13 +0800

    meituanquestion uploaded

---
+++ meituan/meituan6_5.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 20 23:10:41 2017
+
+@author: mywow
+question:
+    最长公共子串
+给定两个字符串的长度s1和s2，找出两个字符串最长公共子串的长度。
+输入：abhike,ebhimg
+输出：bhi
+"""
+
---
+++ meituan/meituan6_6.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 20 23:17:18 2017
+
+@author: mywow
+question:
+合并有序表：
+这里有两个有序表，用自己擅长的语言合并成一个新的有序表，默认升序
+"""
+
