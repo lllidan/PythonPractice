2ce0e57f09f9a940c98f995ff1e3fad4e8408a7c
lllidan <lllidanismine@gmail.com>
2017-09-20 22:32:00 +0800

    upload question

---
+++ tencent/tencent3_1.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 20 21:04:40 2017
+
+@author: mywow
+question:
+编码解码
+【题目描述】假定一种编码的编码范围是a-y的25个字母，从1位到4位的编码，如果我们把该编码按字典
+序排序，形成一个数组如下：
+a,aa,aaa,aaaa,aaab,aaac,.....,....,b,ba,baa,baaa,baab,baac,... ...,yyyw,yyyx,yyyy
+其中a的Index为0，aa的Index为1，aaa的Index为2，以此类推。编写一个函数，输入是任意一个编
+码，输出这个编码对应的index，如：
+输入：
+baca
+输出：
+16331
+参考用例：
+输入：yahy
+输出：390833
+2、输入：qqq
+输出：271250
+"""
+
