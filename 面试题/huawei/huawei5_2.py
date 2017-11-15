d18a00c466e04c62ed7481b614a0aa2f24d5548b
Zexin Young <lllidanismine@gmail.com>
2017-11-15 21:37:20 +0800

    Huawei5.2

--- 面试题/huawei/huawei5_2.py
+++ 面试题/huawei/huawei5_2.py
@@ -15,3 +15,36 @@ question:
 ABBC67%%%CCCAA99
 """
 
+strings = input("请输入一行字符串")
+flag = True
+stringsList = []
+for i in strings:
+    if ord(i)<65 or ord(i)>90 and ord(i)<97 or ord(i)>122:
+        flag = False
+        print("字符"+ i +"不是字符")
+    stringsList.append(i)
+num = 0
+outputs = ""
+stringsListCopy = [i for i in stringsList]
+if flag == True:
+    for p in stringsList:
+        num = 0
+        for q in stringsListCopy:
+            if p == q:
+                num = num + 1
+#                stringsListCopy.remove(q)
+                
+        for i in range(num):
+            stringsListCopy.remove(p)
+        if num != 0:
+            outputs = outputs + p + str(num)
+
+print(outputs)
+        
+        
+        
+        
+
+        
+        
+        
